bb52dab617759630a2bb7be2cc6707603b586445
madrid <abdoelhabiby97@gmail.com>
2020-03-14 01:48:16 +0200

    u4444

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,11 @@ use Illuminate\Support\Facades\Route;
 
 
 
+	 Route::get('/',function(){
+
+	 	 return redirect(route('admin.login'));
+	 });
+
 
 Route::group(['prefix' => 'eldashboard','middleware' => 'authAdmin:admin'],function(){
 
